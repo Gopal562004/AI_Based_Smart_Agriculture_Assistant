8be5df11c43faf4fe47b81341a1e1661d448dc73
Gopal chandrashekhar Gawas <146362802+Gopal562004@users.noreply.github.com>
2024-10-26 11:50:37 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+# TODO: place your cursor at the end of line 5 and press Enter to generate a suggestion.
+# Tip: press tab to accept the suggestion
+
+fake_users = [
+    { "name": "User 1", "id": "user1", "city": "San Francisco", "state": "CA" },
+    { "name": "User 2", "id": "user2", "city": "New York", "state": "NY" },
+    { "name": "User 3", "id": "user3", "city": "Seattle", "state": "WA" }]
